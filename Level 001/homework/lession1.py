d76feffb4d278eeb518c20f18a3de00ecc53cadc
ZukaAbashidze <zukaabashidze70@gmail.com>
2024-12-21 21:53:02 +0400

    Update lession1.py

--- Level 001/homework/lession1.py
+++ Level 001/homework/lession1.py
@@ -1,12 +1,5 @@
-name = "luka"
-surname = "lortkifanidze"
-age = 14
-height = 1.71 
-my = "chemi" 
-sa = "saxeli"
-ir = 'aris'
-im = " me var"
-gb = "da"
+name = "zuka"
+surname ="abashidze"
+age = 27
 
-
-print(my+" "+sa+" "+ir+" "+name +" "+ surname +" "+ im +" "+ str(age)  +" "+ gb  +" "+  im  +" "+ str(height))
+print(name + " " + surname + " " + age )
